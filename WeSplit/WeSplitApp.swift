be6481e7afad7c7a31993969279a31271da4ed07
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2021-06-16 21:24:47 +0500

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Nuriddin Islamov on 16/06/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
